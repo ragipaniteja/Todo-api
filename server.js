d1fa5062c11f59a2d4e7d53cf84f544a1c9d6499
Sai Teja <ragipaniteja93@gmail.com>
2016-07-20 12:33:54 -0500

    Refactored API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT =  process.env.PORT || 3000;
 var todos = [];
@@ -18,20 +20,22 @@ res.json(todos);
 //GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-	//Iterate over todos array. find the match.
-	todos.forEach(function (todo){
-		if(todoId === todo.id){
-		matchedTodo = todo; 
+	var matchedTodo = _.findWhere(todos, {id: todoId});	//underscore lib substitution for the code
+	// var matchedTodo;
+	// //Iterate over todos array. find the match.
+	// todos.forEach(function (todo){
+	// 	if(todoId === todo.id){
+	// 	matchedTodo = todo; 
 		
-		}
-	});
+	// 	}
+	// });
 
 		if(matchedTodo){
 			res.json(matchedTodo);	
 		}else{
 			res.status(404).send();
 		}
+	});
 
 
 
@@ -41,10 +45,17 @@ app.get('/todos/:id', function(req, res){
 	// 	}
 	// }
 	
-});
+
 
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body,'description','completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){	// runs if body.completed is not a boolean and description is not a String
+		return res.status(400).send();
+	}
+
+
+	body.description = body.description.trim();
 	//add id field
 	body.id = todoNextId++;
 

6999fc08761e138aa1230683955028269f3455dd
Sai Teja <ragipaniteja93@gmail.com>
2016-07-21 13:46:40 -0500

    Add variable-playgournd area

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person={
+// 	name: 'Andrew',
+// 	age: 22
+// };
+
+// function updatePerson(obj){
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 37];
+function addGrade (gradesArr){
+	gradesArr.push(50);
+	debugger;
+	//arr = arr.push(40);
+}
+
+addGrade(grades);
+console.log(grades);

bfc33b8eb0358b2b3e7ee393388644c74d86f02b
Sai Teja <ragipaniteja93@gmail.com>
2016-07-23 14:47:35 -0500

    Convert Get /todos/:id to sequelize

--- server.js
+++ server.js
@@ -40,9 +40,21 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	}); //underscore lib substitution for the code
+
+	db.todo.findById(todoId).then(function(todo) {
+		if(!!todo){
+			res.json(todo.toJSON());
+		}else{
+			res.status(404).send();
+		}
+	}, function (e){
+		res.status(500).send();
+	});
+
+
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// }); //underscore lib substitution for the code
 	// var matchedTodo;
 	// //Iterate over todos array. find the match.
 	// todos.forEach(function (todo){
@@ -52,48 +64,23 @@ app.get('/todos/:id', function(req, res) {
 	// 	}
 	// });
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	// if (matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
 });
-
-
-
-// for(var i = 0; i < todos.length; i++){
-// 	if (todos[i].id === todoId){
-// 		res.json(todos[i]);
-// 	}
-// }
-
 
 
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) { // runs if body.completed is not a boolean and description is not a String
-	// 	return res.status(400).send();
-	// }
-
-
-	// body.description = body.description.trim();
-	// //add id field
-	// body.id = todoNextId++;
-
-
-	// //push body into array
-	// todos.push(body);
-
-	// res.json(body);
-
-	//db.todo.create
 	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
 	}, function(e) {
 		res.status(400).json(e);
 	});
 });
+
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {

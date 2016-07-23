05391c2498a42f5e8004528bc5dae3754e53cf8f
Sai Teja <ragipaniteja93@gmail.com>
2016-07-23 15:36:34 -0500

    Convert DELETE /todos to sequelize

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/todos', function(req, res) {
 		res.json(todos);
 	}, function (e){
 		res.status(500).send();
-	})
+	});
 
 
 	// var filteredTodos = todos;
@@ -105,18 +105,35 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted){
+		if(rowsDeleted === 0){
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		}else{
+			res.status(204).send();
+		}
+	}, function (){
+		res.status(500).send();
 	});
 
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (!matchedTodo) {
+	// 	res.status(404).json({
+	// 		"error": "no todo found with that id"
+	// 	});
+	// } else {
+	// 	todos = _.without(todos, matchedTodo);
+	// 	res.json(matchedTodo);
+	// }
 
 });
 

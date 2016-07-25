a620004d0ccdc87f069312d5c1744322ffa75082
Sai Teja <ragipaniteja93@gmail.com>
2016-07-24 20:33:48 -0500

    Add auth token generation to POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 var bcrypt = require('bcryptjs');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
-module.exports = function(sequelize, DataTypes){
+module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
@@ -34,15 +36,15 @@ module.exports = function(sequelize, DataTypes){
 		}
 	}, {
 		hooks: {
-			beforeValidate: function(user, options){
-				if(typeof user.email === 'string'){
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
 		},
 		classMethods: {
-			authenticate: function(body){
-				return new Promise(function(resolve, reject){
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
 					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
 						return reject();
 					}
@@ -66,7 +68,25 @@ module.exports = function(sequelize, DataTypes){
 		instanceMethods: {
 			toPublicJSON: function() {
 				var json = this.toJSON();
-				return  _.pick(json, 'id','email','createdAt','updatedAt');
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({id: this.get('id'), type:type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
+
+					return token;
+				} catch (e) {
+					console.error(e);
+					return undefined;
+				}
 			}
 		}
 	});
